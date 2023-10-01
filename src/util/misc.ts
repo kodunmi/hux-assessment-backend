fcf7ecfac6ece78d8497b6a9278e7ca392116b24
kodunmi <53086177+kodunmi@users.noreply.github.com>
2023-10-01 17:56:09 +0100

    add some helper funcs

--- src/util/misc.ts
+++ src/util/misc.ts
@@ -2,7 +2,6 @@
  * Miscellaneous shared functions go here.
  */
 
-
 /**
  * Get a random number between 1 and 1,000,000,000,000
  */
@@ -19,4 +18,18 @@ export function tick(milliseconds: number): Promise<void> {
       resolve();
     }, milliseconds);
   });
+}
+
+export function validatePhoneNumber(arg: unknown): boolean {
+  // Remove any non-digit characters
+  if (typeof arg !== "string") {
+    return false; // Email must be a string
+  }
+  const cleanedPhoneNumber = arg.replace(/\D/g, "");
+
+  // Check if the cleaned number matches the format
+  // Either starts with '+234' followed by 10 digits
+  // Or starts with '0' followed by 10 digits
+  const regex = /^(?:\+234|0)\d{10}$/;
+  return regex.test(cleanedPhoneNumber);
 }
